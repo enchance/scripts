00a5150edebe08bb50d4e23bdf09f9aad66bc32a
enchance <enchance@gmail.com>
2023-12-20 17:06:12 +0800

    allowing merging of folders from multiple folder locations

--- scripts/merge.py
+++ scripts/merge.py
@@ -3,11 +3,13 @@
 import os, sys, shutil, click  # noqa
 from pathlib import Path
 from rich import print
+from icecream import IceCreamDebugger
 
 from utils.utils import command_config, path_config
 
 
 __version__ = '0.3.0'
+ic = IceCreamDebugger(prefix='')
 errors = {}
 
 
@@ -21,38 +23,45 @@ def cleanup_folders(path: str):
 
 @click.command(**command_config)
 @click.version_option(__version__, prog_name='mergefiles')
-@click.argument('input_path', type=path_config)
+@click.argument('input_paths', type=path_config, nargs=-1)
 @click.option('--prefix', '-p', help='Prefix of each folder chunked folder', default='chunk-', show_default=True)
-@click.option('--output', '-o', type=path_config, help='Output path to create subfolders in')
-def main(input_path: Path, prefix: str, output: Path):
+@click.option('--output', '-o', type=path_config, help='Output path to create subfolders in', default='.',
+              show_default=True)
+def main(input_paths: tuple[Path], prefix: str, output: Path):
     """
     Collate all files in subfolders that have a specific prefix. Only searches for first-level subfolders.\n
     Works with the chunkfiles script.
     """
-    folder_path = input_path
-    output_path = output or folder_path
+    output_path = output
     counter = 0
 
-    chunk_folders = [i for i in os.listdir(folder_path) if is_valid_folder(prefix, i)]
+    chunk_folders: list[str] = []
+    for path in input_paths:
+        chunk_folders.extend([i for i in os.listdir(path) if is_valid_folder(prefix, i)])
 
-    for folder in chunk_folders:
-        subfolder_path = os.path.join(folder_path, folder)
-        counter += len(os.listdir(subfolder_path))
-        for file in os.listdir(subfolder_path):
-            from_path = os.path.join(folder_path, folder, file)
-            to_path = os.path.join(output_path, file)
+    for folder_path in input_paths:
+        for folder in chunk_folders:
+            subfolder_path = os.path.join(folder_path, folder)
+
+            if not os.path.isdir(subfolder_path):
+                continue
+
+            counter += len(os.listdir(subfolder_path))
+            for file in os.listdir(subfolder_path):
+                from_path = os.path.join(folder_path, folder, file)
+                to_path = os.path.join(output_path, file)
+
+                try:
+                    shutil.move(from_path, to_path)
+                except Exception:   # noqa
+                    errors.setdefault('unmoved', [])
+                    errors['unmoved'].append(from_path)
 
             try:
-                shutil.move(from_path, to_path)
+                cleanup_folders(subfolder_path)
             except Exception:   # noqa
-                errors.setdefault('unmoved', [])
-                errors['unmoved'].append(from_path)
-
-        try:
-            cleanup_folders(subfolder_path)
-        except Exception:   # noqa
-            errors.setdefault('undeleted', [])
-            errors['undeleted'].append(subfolder_path)
+                errors.setdefault('undeleted', [])
+                errors['undeleted'].append(subfolder_path)
 
     if len(errors):
         print(errors)
